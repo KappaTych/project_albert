a0da5b16006071a1359913fddc348bfaf6e6cd63
pototckaia <pototckaia@gmail.com>
2019-11-13 23:23:30 +1000

    Move cleanup to DebugMessageSystem

--- Assets/Scripts/Systems/Debug/DebugMessageSystem.cs
+++ Assets/Scripts/Systems/Debug/DebugMessageSystem.cs
@@ -2,10 +2,13 @@ using System.Collections.Generic;
 using Entitas;
 using UnityEngine;
 
-public class DebugMessageSystem : ReactiveSystem<GameEntity>
+public class DebugMessageSystem : ReactiveSystem<GameEntity>, ICleanupSystem
 {
+    readonly IGroup<GameEntity> _debugMessages;
+
     public DebugMessageSystem(Contexts contexts) : base(contexts.game)
     {
+        _debugMessages = contexts.game.GetGroup(GameMatcher.DebugMessage);
     }
 
     protected override ICollector<GameEntity> GetTrigger(IContext<GameEntity> context)
@@ -23,6 +26,14 @@ public class DebugMessageSystem : ReactiveSystem<GameEntity>
         foreach (var e in entities)
         {
             Debug.Log(e.debugMessage.message);
+        }
+    }
+
+    public void Cleanup()
+    {
+        foreach (var e in _debugMessages.GetEntities())
+        {
+            e.Destroy();
         }
     }
 }
--- Assets/Scripts/Systems/Debug/TutorialSystems.cs
+++ Assets/Scripts/Systems/Debug/TutorialSystems.cs
@@ -4,9 +4,7 @@ public class TutorialSystems : Feature
 {
     public TutorialSystems(Contexts contexts) : base("Tutorial Systems")
     {
-        Add(new HelloWorldSystem(contexts));
         Add(new LogMouseClickSystem(contexts));
         Add(new DebugMessageSystem(contexts));
-        Add(new CleanupDebugMessageSystem(contexts));
     }
 }
