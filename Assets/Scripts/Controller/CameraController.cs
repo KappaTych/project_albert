44ede1de95d86b1f6c47f0e3c28fdb29d55c1eaf
pototckaia <pototckaia@gmail.com>
2019-12-18 16:20:49 +1000

    fix camera

--- Assets/Scripts/Controller/CameraController.cs
+++ Assets/Scripts/Controller/CameraController.cs
@@ -4,19 +4,21 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-	private Transform target;
-	[SerializeField] private float smoothMoveSpeed;
+    private Transform target;
+    [SerializeField] private float smoothMoveSpeed;
+    // Start is called before the first frame update
     private void Start()
     {
-		target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+        target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
     }
 
+    // Update is called once per frame
     private void LateUpdate()
     {
-		//MARKER Method 1. traditional
-		//transform.position = new Vector3(target.position.x, target.position.y, transform.position.z);
-			
-		//MARKER Method 2. Smooth
-		transform.position = Vector3.Lerp(transform.position, new Vector3(target.position.x, target.position.y, transform.position.z), Time.deltaTime * smoothMoveSpeed);
+        //MARKER Method 1. traditional
+        //transform.position = new Vector3(target.position.x, target.position.y, transform.position.z);
+
+        //MARKER Method 2. Smooth
+        transform.position = Vector3.Lerp(transform.position, new Vector3(target.position.x, target.position.y, transform.position.z), Time.deltaTime * smoothMoveSpeed);
     }
 }
