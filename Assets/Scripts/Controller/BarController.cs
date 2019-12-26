e361a6292a4023628630665ee2fc895caac3ee1f
pototckaia <pototckaia@gmail.com>
2019-12-27 02:18:55 +1000

    add simple health bar

---
+++ Assets/Scripts/Controller/BarController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BarController : MonoBehaviour
+{
+    public float fillSpeed = 0.5f;
+    
+    private Transform bar;
+    private float targetScale = 1;
+    void Start()
+    {
+        bar = transform.Find("Fill Area");
+    }
+
+    private void Update()
+    {
+        if (bar.localScale.x < targetScale)
+            return;
+        SetScale(bar.localScale.x - fillSpeed * Time.deltaTime);
+    }
+
+    public void SetProggress(float s)
+    {
+        targetScale = bar.localScale.x - s;
+    }
+
+    private void SetScale(float s)
+    {
+        bar.localScale = new Vector3(s, 1);
+    }
+}
