7adb75722c2f5b0d3bbcd8e1de2341febe96f299
pototckaia <pototckaia@gmail.com>
2020-01-05 01:43:30 +1000

    add health component

---
+++ Assets/Scripts/Components/HellthComponent.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using Entitas;
+using Entitas.CodeGeneration.Attributes;
+
+[Core, Event(EventTarget.Self)]
+public class Hellth : IComponent
+{
+    public float curValue;
+    public float maxValue;
+}
