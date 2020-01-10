d725454d2dce3091b3b71873222d7d1b6530c782
pototckaia <pototckaia@gmail.com>
2020-01-10 17:16:24 +1000

    add attack event on enemy animation

--- Assets/Scripts/AI/RangedEnemyAI.cs
+++ Assets/Scripts/AI/RangedEnemyAI.cs
@@ -4,15 +4,8 @@ using UnityEngine;
 
 public class RangedEnemyAI : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    void Attack()
     {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
