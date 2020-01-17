c2f390f39651fcf3e253e3d6847cd2bf74c904bd
pototckaia <pototckaia@gmail.com>
2020-01-17 12:41:23 +1000

    fix SampleScene.unity

--- Assets/Scripts/Controller/PlayerStatController.cs
+++ Assets/Scripts/Controller/PlayerStatController.cs
@@ -10,6 +10,7 @@ public class PlayerStatController : MonoBehaviour
     void Awake()
     {
         var contexts = Contexts.sharedInstance;
+        contexts.SubscribeId();
         var entity = contexts.core.CreateEntity();
 
         gameObject.Link(entity);
